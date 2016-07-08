daf294643f9fa97069e34ea3fe773bcdbcc4080a
californianaseabass <releasetheseabass@gmail.com>
2016-07-07 23:44:36 -0400

    updated to node 6.x.x and support for recursive search of photos

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
  * Created by californianseabass on 2/17/15.
  */
 
- var PREFIX = '/dyn_photos/';
+ var PREFIX = '/photos/';
 
 var express = require('express');
 var app = express();
 
+var _ = require('lodash');
 var fs = require('fs');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -22,70 +23,93 @@ http.listen(3000, function(){
 });
 
 if (process.argv.length < 3) {
-	console.log('node server.js $DIRNAME');
-	process.exit(-1);
+  console.log('node server.js $DIRNAME');
+  process.exit(-1);
+}
+
+// Script Globals
+var photo_directory = process.argv[2]; //input directory given by launch command
+var htmlToFs = {}; // key value of the html file route to the actual location in filesystem
+
+fetch_photos = function(dir, cb) {
+  fs.readdir(dir, function(err, files) {
+    if (files === undefined) {
+      console.error('Error trying to read files from directory: ' + files);
+      return
+;   }
+
+    var jpg_files = files.filter(f => path.extname(f).toLowerCase() === '.jpg')
+      .map(f => {
+        htmlToFs[f] = path.join(dir, f);
+        const htmlPath = path.join(PREFIX, f);
+        return htmlPath;
+      });
+
+    const dirs = files.filter(f => fs.lstatSync(path.join(dir, f)).isDirectory());
+    const nDirs = dirs.length;
+    let nDone = 0;
+    let deepFiles1 = [];
+
+    if (nDirs > 0) {
+      dirs.forEach(d => fetch_photos(path.join(dir, d), fs => {
+        nDone++;
+        deepFiles1 = deepFiles1.concat(fs);
+        if (nDone === nDirs) {
+          cb(jpg_files.concat(deepFiles1));
+        }
+
+      }));
+    } else {
+      cb(jpg_files);
+    }
+  });
 }
 
 
-var photo_directory = process.argv[2];
-
-var fetch_photos = function(directory) {
-	fs.readdir(directory, function(err, files) {
-		if (files === undefined) {
-			console.log('Error trying to read files from directory: ' + files);
-			return;
-		}
-		var filtered_files = files.filter(function(file) {
-			return path.extname(file).toLowerCase() === '.jpg' && (file.search(/._/) === -1);
-		});
-		files = filtered_files.map(function(file) {
-			return path.join(PREFIX, file);
-		});
-		io.emit('ls', {'files': files});
-	 	return;
-	});
+var send_photos_to_client = function(directory) {
+  fetch_photos(directory, files => io.emit('ls', {'files': files}));
+  return;
 }
 
 // setup an endpoint for the client to request photos from
 app.get(PREFIX +':photo', function(req, res){
-    var filename = req.params.photo;
+    const htmlPath = req.params.photo;
+    const fsPath = htmlToFs[htmlPath] ? htmlToFs[htmlPath] : '';
+    const sendfile_options = { root: path.dirname(fsPath) };
 
-    var sendfile_options = {
-    	root: photo_directory
-    };
-    res.sendFile(filename, sendfile_options, function (err) {
-	    if (err) {
-	      console.log(err);
-	      res.status(err.status).end();
-	    }
-	    else {
-	      console.log('Sent: ', filename);
-	    }
-	  });
+    if (fs.stat(fsPath, (err, stats) => {
+      if (err) {
+      res.status(404);
+        res.send(err.text);
+        return;
+      }
+      if (stats.isFile()) {
+         res.sendFile(fsPath, function (err) {
+          if (err) {
+            console.log(err);
+            res.status(err.status).end();
+          }
+          else {
+            console.log('Sent: ', fsPath);
+          }
+      });
+      } else {
+        res.status(404);
+        res.send('Could not find requested photo: ' + re.params.photo);
+      }
+      return;
+    }));
 });
 
 io.on('connection', function(socket) {
-	console.log('Connecting established.');
+  console.log('Connection established.');
 
-	socket.on('delete', function (data) {
-		var path_to_delete = photo_directory + '/' + data.photo.split(PREFIX)[1]
-		fs.unlink(path_to_delete, function(e) {
-			console.log('deleted: ' + path_to_delete);
-		});
+  socket.on('delete', function (data) {
+    var path_to_delete = photo_directory + '/' + data.photo.split(PREFIX)[1]
+    fs.unlink(path_to_delete, function(e) {
+      console.log('deleted: ' + path_to_delete);
+    });
   });
 
-	fs.readdir(photo_directory, function(err, files) {
-		if (files === undefined) {
-			console.log('Error trying to read files from directory: ' + files);
-			return;
-		}
-		var filtered_files = files.filter(function(file) {
-			return path.extname(file).toLowerCase() === '.jpg' && (file.search(/._/) === -1);
-		});
-		files = filtered_files.map(function(file) {
-			return path.join(PREFIX, file);
-		});
-		io.emit('ls', {'files': files});
-	 	return;
-	});
+  send_photos_to_client(photo_directory);
 });
